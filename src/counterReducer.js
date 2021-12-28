fa2587f66370f5aaec184300616d8fa8bfb94161
Eleonora <eleonora1weber@gmail.com>
2021-12-28 21:49:36 +0100

    added and counterContainer

---
+++ src/counterReducer.js
@@ -0,0 +1,18 @@
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case "INCREMENT10":
+      return state + 10;
+    case "DECREMENT10":
+      return state - 10;
+    case "RESET":
+      return 0;
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
